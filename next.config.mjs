ddc9839119b750828e6f19329dcae0fa59355b51
Reeshav Acharya <reeshav99acharya@gmail.com>
2025-10-11 10:10:24 -0400

    Build and deploy fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // Required for static export
+  images: {
+    unoptimized: true, // Needed for GitHub Pages
+  },
+  basePath: '', 
+  assetPrefix: '', 
+};
 
 export default nextConfig;
